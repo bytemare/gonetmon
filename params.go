ff673f206257640ccd7354dae34a29e7d95a1148
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-07 17:12:04 +0200

    updated necessary parameters

--- params.go
+++ params.go
@@ -6,29 +6,23 @@ type Parameters struct {
 
 	// Raw data parameters
 	Filter			string	// BPF filter to filter traffic sniffing
-	CollectorFile	string 	// File the Collector dumps data in, and the Monitor reads from
 
 	// Analysis related parameters
-	ProbePeriod			int	// Time (seconds) between analyses of collected data
 	AlertSpan			int	// Time (seconds) span to monitor for alert trigger
 	AlertThreshold		int	// Traffic (hits/span) threshold triggering an alert
 
 	// Display related parameters
 	DisplayRefresh	int		// Time (seconds) the display will be updated
-	DisplayFormat	string	// Format of result of allowedFormats
-	Output			string	// Output destinations among allowedOutputs
+	DisplayType		string	// Type of display (for now it's CLI only)
 }
 
 // Default values for Parameter object
 const (
 	defFilter			=	"tcp and port 80"
-	defCollectorFile	=	"./gonetmon.dump"
-	defProbePeriod 		=	1
 	defAlertSpan		=	120
 	defAlertThreshold	=	500
 	defDisplayRefresh	=	10
-	defDisplayFormat	=	"plain"
-	defOutput			=	"cli"
+	defDisplayType		=	"console"
 )
 
 // LoadParams loads the application's parameters it should run on into an object and returns it
@@ -36,12 +30,10 @@ func LoadParams() *Parameters{
 	// Todo : There should be a better way of doing this + argument validation
 
 	return &Parameters{
-		CollectorFile:	defCollectorFile,
-		ProbePeriod:	defProbePeriod,
+		Filter:			defFilter,
 		AlertSpan:		defAlertSpan,
 		AlertThreshold:	defAlertThreshold,
 		DisplayRefresh:	defDisplayRefresh,
-		DisplayFormat:	defDisplayFormat,
-		Output: 		defOutput,
+		DisplayType:	defDisplayType,
 	}
 }
