958819b02dbe8a69f2a50444937e05bcd946e676
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-07 14:07:08 +0200

    added monitor func

---
+++ monitor.go
@@ -0,0 +1,21 @@
+package main
+
+
+
+
+
+
+
+
+
+
+
+// Monitor is a goroutine that listen on the dataChan channel to pull datapackets for analysis
+func Monitor(parameters *Parameters, dataChan <-chan dataMsg, syncChan <-chan struct{}){
+
+
+
+
+
+
+}
