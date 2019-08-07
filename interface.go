e15efc7e6ee62cbe5a52a278c8031a4311e100a6
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-07 20:00:36 +0200

    renamed function that somehow did not want to compile

--- interface.go
+++ interface.go
@@ -1,4 +1,4 @@
-//Interface is a goroutine that allows an operator to interact with the tool through CLI.
+//command is a goroutine that allows an operator to interact with the tool through CLI.
 //
 //Implemented Commands :
 //- stop
@@ -11,21 +11,19 @@ import (
 	"syscall"
 )
 
-// Interface handles CLI interactions
-func Interface(syncChan chan<- struct{}, nbReceivers int) {
-
+// command handles CLI interactions
+func command(syncChan chan<- struct{}, nbReceivers int) {
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	for sig := range sigs {
-		fmt.Println("Interface received signal :", sig.String())
+		fmt.Println("command received signal :", sig.String())
 		for n := 0; n < nbReceivers ; n++ {
 			syncChan <- struct{}{}
 		}
 		break
 	}
 
-	fmt.Print("Interface terminating\n")
-
+	fmt.Print("command terminating\n")
 }
