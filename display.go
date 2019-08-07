3f54f0fc3261a028aee2f47d4c31c151a58f157e
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-07 20:05:18 +0200

    Added display routine

---
+++ display.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+
+
+func outputReport(r *reportMsg, output string){
+	// TODO
+	fmt.Printf("[i] Display received a report to '%s' : '%s' !\n", output, reportMsg{})
+}
+
+
+func Display(parameters *Parameters, reportChan <-chan reportMsg, alertChan <-chan alertMsg, syncChan <-chan struct{}){
+
+	displayloop:
+	for {
+
+		select {
+
+		case <-syncChan:
+			fmt.Println("[i] Display received sync message")
+			break displayloop
+
+		case alert := <-alertChan:
+			fmt.Printf("[ALERT] %s\n", alert.body)
+
+		case report := <-reportChan:
+
+			// Interpret report and adapt to desired output
+			outputReport(&report, parameters.DisplayType)
+		}
+	}
+}
