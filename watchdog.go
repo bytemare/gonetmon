aa61eda3393fb6099847e05b39921bb8552836fa
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-08 21:35:49 +0200

    Implemented a time-based LRU cache and dedicated goroutine to observe traffic behaviour and alert when threshold is met

---
+++ watchdog.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	defaultTick		= 500 * time.Millisecond
+)
+
+type HitCache struct {
+
+	// Channels to send operations on
+	push    chan time.Time
+	bufSize uint // size of channels
+
+	// Doubly linked list
+	list		list.List
+
+	// Number of elements in current list
+	size		uint
+}
+
+type Watchdog struct {
+	
+	// Cache to store timely identified hits and time window to keep them
+	cache		HitCache
+	window		time.Duration
+	tick		time.Duration
+	
+	// Threshold above which an alert will be raised
+	threshold	uint
+	
+	// Channel to send alerts to
+	alertChan	chan<-	alertMsg
+	
+	// Current state of alert
+	alert		bool
+
+	// Synchronisation
+	stop		<-chan	struct{}
+	wg			*sync.WaitGroup
+}
+
+func (w *Watchdog) Hits() int{
+	return int(w.cache.size)
+}
+
+func buildAlertMsg(w *Watchdog, recovery bool, t time.Time) alertMsg {
+
+	var message string
+
+	if recovery {
+		message = fmt.Sprintf(defRecoveryFormat, t.String())
+	} else {
+		message = fmt.Sprintf(defAlertFormat, w.Hits(), t.String())
+	}
+
+	return alertMsg{
+		recovery:  recovery,
+		body:      message,
+		timestamp: time.Time{},
+	}
+}
+
+// addHit adds an element to the cache by sending a push request to the goroutine
+func (w *Watchdog) addHit(t time.Time) {
+	w.cache.push <- t
+}
+
+
+
+// Verify checks the cache, raising or lowering the alert and sending a message if necessary
+func (w *Watchdog) verify() {
+
+	// If the cache is empty, no need to go further
+	if w.cache.list.Len() <= 0 {
+		// If we were previously in alert, deescalate and send recovery message
+		if w.alert {
+			w.alert = false
+			w.alertChan <- buildAlertMsg(w, true, time.Now())
+			return
+		}
+	}
+
+	// New Alert
+	if w.cache.size >= w.threshold && !w.alert {
+			w.alert = true
+			w.alertChan <- buildAlertMsg(w, false, time.Now())
+	} else {
+		// Recovery
+		if w.alert {
+			w.alert = false
+			w.alertChan <- buildAlertMsg(w, true, time.Now())
+		}
+	}
+
+	return
+}
+
+// Evict pops all values from the cache that have passed the authorised window
+func (w *Watchdog) evict(now time.Time){
+	defer w.verify()
+
+	if w.cache.list.Len() <= 0 {
+		return
+	}
+
+	for {
+		e := w.cache.list.Front()
+
+		// If the element is older than allowed window
+		if now.Sub(e.Value.(time.Time)) > w.window {
+			w.cache.list.Remove(e)
+			w.cache.size--
+		} else {
+			// Since we store timed values incrementally, following values are all still valid
+			break
+		}
+	}
+}
+
+
+// NewWatchdog returns a watchdog struct and launches a goroutine that will observe its cache to detect alert triggering
+func NewWatchdog(window time.Duration, tick time.Duration, threshold uint, c chan<- alertMsg, bufSize uint, stop <-chan struct{}, wg *sync.WaitGroup) *Watchdog {
+	dog := Watchdog{
+		cache: HitCache{
+			push:    make(chan time.Time, bufSize),
+			bufSize: bufSize,
+			list:    list.List{},
+			size:    0,
+		},
+		window:    window,
+		tick: tick,
+		threshold: threshold,
+		alertChan: c,
+		alert:     false,
+		stop:      stop,
+		wg:        wg,
+	}
+
+	if tick == 0 {
+		dog.tick = defaultTick
+	}
+
+
+	ticker := time.NewTicker(dog.tick)
+
+	// Routine that continuously verifies the cache and will inform about alert status
+	go func() {
+		for {
+			select {
+
+			// Continuously evict old elements
+			case t := <-ticker.C:
+				dog.Evict(t)
+
+			// Push request
+			case p := <-dog.cache.push:
+				dog.cache.list.PushBack(p)
+				dog.cache.size++
+
+				dog.Verify()
+			}
+		}
+	}()
+
+	return &dog
+}
