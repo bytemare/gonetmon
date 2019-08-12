5bcf1039c3a0bdd7da3e6535e2861fb0e9ee9b1e
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-13 00:49:54 +0200

    cleaned up and commented

--- cli.go
+++ cli.go
@@ -1,8 +1,8 @@
-//command is a goroutine that allows an operator to interact with the tool through CLI.
+// CLI acts as a command interface that allows an operator to interact with the tool through CLI.
 //
-//Implemented Commands :
-//- stop
-package gonetmon
+//Implemented commands :
+//- stop : through SIGINT or SIGTERM signals
+package main
 
 import (
 	"io"
@@ -11,25 +11,24 @@ import (
 	"syscall"
 )
 
-// command handles CLI interactions
-func command(syn *Sync) {
+// CLI handles command line interactions
+func CLI(syn *Sync) {
 	defer syn.wg.Done()
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	for sig := range sigs {
-		log.Info("Command received signal :", sig.String())
-		// This Goroutine is not waiting for a stop signal/message, so we take one off
-
+		log.Info("CLI received signal :", sig.String())
 		log.SetOutput(io.MultiWriter(os.Stdout, log.Out))
 		log.Info("Logging to both file and console.")
 
+		// This Goroutine is not waiting for a stop signal/message, so we take one off
 		for n := 1; n < int(syn.nbReceivers); n++ {
 			syn.syncChan <- struct{}{}
 		}
 		break
 	}
 
-	log.Info("Command terminating.")
+	log.Info("CLI terminating.")
 }
