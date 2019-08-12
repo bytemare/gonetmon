c848e54ffd6d47d7e5083a8a2348e6a35b0092f4
bytemare <{ID}+{username}@users.noreply.github.com>
2019-08-13 01:12:42 +0200

    added comment

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// gonetmon is a network monitoring tool.
+// It captures packets on the wire from devices based on given criteria, and displays statistics about traffic.
 package main
 
 import "os"
